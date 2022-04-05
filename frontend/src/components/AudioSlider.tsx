b83a3f12a9f8992808428263141139f3f8ccbd2b
Hugo Mårdbrink <hugo@mardbrink.se>
2022-04-05 16:42:01 +0200

    Added UI for Nav bar and Bottom bar

---
+++ frontend/src/components/AudioSlider.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function AudioSlider() {
+    return (
+        <div className="text-white">
+            Audio Slider!
+        </div>
+    );
+}
+
+export default AudioSlider;
---
+++ frontend/tailwind.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  content: ["./src/**/*.{css,js,jsx,ts,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
